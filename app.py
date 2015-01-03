8ad69886bd66df7c7cf97807d65b1d1c5cd0a1ee
Aida Piccato <aidapiccato@gmail.com>
2015-01-03 11:52:18 -0500

    Created separat register pages for tutors and tutees

--- app.py
+++ app.py
@@ -11,33 +11,36 @@ db = conn['users']
 def index():
 	return render_template("base.html")
 
-def create_account(email, password, first_name, last_name):
-	new_user = {
-		'email': email,
-		'password': password,
-		'first_name': first_name,
-		'last_name': last_name,
-	}
-	return db.accounts.insert(new_user)
+def create_account(user_type, account):
+	if user_type == "tutor":
+		return db.tutors.insert(account)
+	return db.tutees.insert(account)
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
+@app.route("/register/<user_type>", methods=["GET", "POST"])
+def register(user_type):
+	base_url = "register_" + user_type + ".html"
 	if request.method == "GET":
-		return render_template("register.html")
+		return render_template(base_url)
 	else:
-		first_name = request.form["first_name"]
-		last_name = request.form["last_name"]
-		email = request.form["email"]
-		password = request.form["password"]
+		account = {}
+		account['first_name'] = request.form["first_name"]
+		account['last_name'] = request.form["last_name"]
+		account['email'] = request.form["email"]
+		account['password'] = request.form["password"]
 		confirm_password = request.form["confirm_password"]
+		account['school'] = request.form["school"]
+		account['grade'] = request.form["grade"]
+		if user_type == "tutor":
+			account['courses'] = request.form["courses"]
+			account['subjects'] = request.form["subjects"]
 		if request.form['b'] == "Submit":
-			if password == confirm_password:
-				create_account(email, password, first_name, last_name)
-				flash("You have succesfully created an account")
+			if account['password'] == confirm_password:
+				create_account(user_type, account)
+				flash(user_type + ": You have succesfully created an account")
 				return render_template("base.html")
 			else:
 				flash("Passwords do not match")
-				return render_template("register.html")
+				return render_template(base_url)
 
 if __name__ == "__main__":
 	app.debug = True
