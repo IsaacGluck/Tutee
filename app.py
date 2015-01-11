ed17b1e4557c3b131c16638f2f158bcdc6c24393
CobyGoldbergs <coby.goldberg@gmail.com>
2015-01-11 11:34:02 -0500

    check basic features plus sprinkles for scores, still need address/ timing

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo import Connection
 import hashlib, uuid
 import random
 
+
 app = Flask(__name__)
 
 ## Mongo ##
@@ -14,21 +15,25 @@ names = ['thluffy','dennis','bucky','doughjoe',
          'victor','jesus', 'coby', 'isaac', 'aida', 'leslie', 'z', 'cat', 'lou', 'jake', 'fred', 'bob', 'lee', 'rob', 'ulyses', 'jackson', 'stone']
 school = ['Stuyvesant', 'Bard', 'Bronx Science']
 grade = [9, 10, 11, 12]
-subs = ['Chemistry', 'English', 'Physics', 'Biology']
+subs = ['chemistry', 'English', 'Physics', 'Biology']
 dates = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
 
 dlist = []
-for i in range(20):
+for i in range(30):
         d = {'first_name':random.choice(names),
              'school':random.choice(school),
              'grade':random.choice(grade),
-             'subjects':[random.choice(subs), random.choice(subs)],
-             'days':[random.choice(dates), random.choice(dates)],
-             
+             '%s'%random.choice(subs):True,
+             '%s'%random.choice(dates):'1-8',
+             '%s'%random.choice(dates):'1-8',
         }
         dlist.append(d)
 
-db.tutors.insert(dlist)
+#db.tutors.insert(dlist)
+
+#db.tutors.remove({})
+
+
 
 tut = {
         'first_name':random.choice(names),
@@ -61,9 +66,18 @@ def register_user(user_type, form):
         account['grade'] = form["grade"]
         if user_type == "tutor":
                 account['courses'] = form["courses"]
-                account['subjects'] = form["subjects"]
-                account['times'] = form["times"]
+                #for each subject a tutor lists, it will have a seperate element in the dictionary with value "True"
+                for subject in form['subjects']:
+                        account['%s' % subject] = True
+                times = form["times"]
+                td = times.split(";")
+                x = 0
+                #each day is given seperate element with value times
+                while x < len(td):
+                        account['%s' % td[x]] = td[x+1]
+                        x += 2
                 account['match_score'] = 0
+        print account
         return account
 
 def create_account(user_type, account):
@@ -89,25 +103,18 @@ def search_operation(course, subject, times):
         for time in times:
                 t = time.split(";")
                 days.append(t[0])
-        #create list tutors filled with all tutors with right subject and free day(s)
-
-        print subject
-
+        #create list tutor_list filled with all tutors with right subject and free day(s)
         for day in days:
-                tutor_list = db.tutors.find({"subjects.first":subject})
+                tutor_list = db.tutors.find({"%s"%subject:True, "%s"%day:{ '$exists': True }})
         
         print("COUNTTTTTTTTT: " + str(tutor_list.count()))
                 
         #for each tutor on the new list, give them a score based on secondary features
         for tutor in tutor_list:
-                print tutor
                 score = 0
-                #print(tutor['school'])
-                #if tutor['school'] == tut['school']:
-                 #       score += 1
-                # it's a bit better if they go to the same school
-                #score += (tutor['grade'] - tut['grade'])/5
-                #An older tutor is preferable
+                if tutor['school'] == tut['school']:
+                        score += 1 # one point for going to the same school
+                score += (tutor['grade'] - tut['grade'])/5 #An older tutor is preferable
 
                 
 
@@ -139,16 +146,16 @@ def search():
                 for d in days:
                         hour = request.form["%s" % d + "_Time"]
                         new_req = d + ";" + hour
-                        addresses = request.form.getlist("%s" % d + "_Address")
-                        for a in addresses:
-                                new_req += ";" + tut['address'] #to be replaced with sessions use to find the current user's home/ school address
-                        other_add = request.form["%s" % d + "_Other"]
-                        if other_add:
-                                new_req += ";" + other_add
+                        #addresses = request.form.getlist("%s" % d + "_Address")
+                       # for a in addresses:
+                           #     new_req += ";" + tut['address'] #to be replaced with sessions use to find the current user's home/ school address
+                        #other_add = request.form["%s" % d + "_Other"]
+                        #if other_add:
+                         #       new_req += ";" + other_add
                         times.append(new_req)
-                tutor_list = search_operation(courses, subjects, times)
                 if request.form['b'] == "Submit":
-                        flash(times)
+                        tutor_list = search_operation(courses, subjects, times)
+                        flash(tutor_list)
                         return render_template("base.html") #Will redirect to a search return page, temp for testing purposes of returns
                         
 
